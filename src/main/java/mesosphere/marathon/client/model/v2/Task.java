0bbfb6d6accfda663ea76242d7188ad94f000ba3
Lewis Headden <lewis_headden@condenast.com>
2017-02-03 17:25:26 -0500

    Convert spaces to tabs

--- src/main/java/mesosphere/marathon/client/model/v2/Task.java
+++ src/main/java/mesosphere/marathon/client/model/v2/Task.java
@@ -5,90 +5,90 @@ import mesosphere.marathon.client.utils.ModelUtils;
 import java.util.Collection;
 
 public class Task {
-    private String host;
-    private String id;
-    private String appId;
-    private Collection<Integer> ports;
-    private String stagedAt;
-    private String startedAt;
-    private Collection<HealthCheckResult> healthCheckResults;
-    private String version;
-    private String state;
+	private String host;
+	private String id;
+	private String appId;
+	private Collection<Integer> ports;
+	private String stagedAt;
+	private String startedAt;
+	private Collection<HealthCheckResult> healthCheckResults;
+	private String version;
+	private String state;
 
-    public String getHost() {
-        return host;
-    }
+	public String getHost() {
+		return host;
+	}
 
-    public void setHost(String host) {
-        this.host = host;
-    }
+	public void setHost(String host) {
+		this.host = host;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getAppId() {
-        return appId;
-    }
+	public String getAppId() {
+		return appId;
+	}
 
-    public void setAppId(String appId) {
-        this.appId = appId;
-    }
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
 
-    public Collection<Integer> getPorts() {
-        return ports;
-    }
+	public Collection<Integer> getPorts() {
+		return ports;
+	}
 
-    public void setPorts(Collection<Integer> ports) {
-        this.ports = ports;
-    }
+	public void setPorts(Collection<Integer> ports) {
+		this.ports = ports;
+	}
 
-    public String getStagedAt() {
-        return stagedAt;
-    }
+	public String getStagedAt() {
+		return stagedAt;
+	}
 
-    public void setStagedAt(String stagedAt) {
-        this.stagedAt = stagedAt;
-    }
+	public void setStagedAt(String stagedAt) {
+		this.stagedAt = stagedAt;
+	}
 
-    public String getStartedAt() {
-        return startedAt;
-    }
+	public String getStartedAt() {
+		return startedAt;
+	}
 
-    public void setStartedAt(String startedAt) {
-        this.startedAt = startedAt;
-    }
+	public void setStartedAt(String startedAt) {
+		this.startedAt = startedAt;
+	}
 
-    public Collection<HealthCheckResult> getHealthCheckResults() {
-        return healthCheckResults;
-    }
+	public Collection<HealthCheckResult> getHealthCheckResults() {
+		return healthCheckResults;
+	}
 
-    public void setHealthCheckResults(Collection<HealthCheckResult> healthCheckResults) {
-        this.healthCheckResults = healthCheckResults;
-    }
+	public void setHealthCheckResults(Collection<HealthCheckResult> healthCheckResults) {
+		this.healthCheckResults = healthCheckResults;
+	}
 
-    public String getVersion() {
-        return version;
-    }
+	public String getVersion() {
+		return version;
+	}
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
+	public void setVersion(String version) {
+		this.version = version;
+	}
 
-    public String getState() {
-        return state;
-    }
+	public String getState() {
+		return state;
+	}
 
-    public void setState(String state) {
-        this.state = state;
-    }
+	public void setState(String state) {
+		this.state = state;
+	}
 
-    @Override
-    public String toString() {
-        return ModelUtils.toString(this);
-    }
+	@Override
+	public String toString() {
+		return ModelUtils.toString(this);
+	}
 }
