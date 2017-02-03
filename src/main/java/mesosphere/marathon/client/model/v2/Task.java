298f2e0e981dc4b8b04db0c6b94d00a951d8c7bf
Lewis Headden <lewis_headden@condenast.com>
2017-02-03 17:41:43 -0500

    Organize imports

--- src/main/java/mesosphere/marathon/client/model/v2/Task.java
+++ src/main/java/mesosphere/marathon/client/model/v2/Task.java
@@ -1,8 +1,8 @@
 package mesosphere.marathon.client.model.v2;
 
-import mesosphere.marathon.client.utils.ModelUtils;
-
 import java.util.Collection;
+
+import mesosphere.marathon.client.utils.ModelUtils;
 
 public class Task {
 	private String host;
