aa411723ba42524275cd5d66c1531be7e841b88e
TT044106 <Trevin.Teacutter@Cerner.com>
2017-02-24 14:09:52 -0600

    Changes to support DCOS extending the Marathon interface.

--- src/main/java/mesosphere/marathon/client/Marathon.java
+++ src/main/java/mesosphere/marathon/client/Marathon.java
@@ -2,9 +2,11 @@ package mesosphere.marathon.client;
 
 import java.util.List;
 
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
+import mesosphere.client.common.ClientUtils;
 import mesosphere.marathon.client.model.v2.App;
 import mesosphere.marathon.client.model.v2.AppWithVersion;
 import mesosphere.marathon.client.model.v2.DeleteAppTaskResponse;
@@ -25,86 +27,107 @@ import mesosphere.marathon.client.model.v2.VersionsResponse;
 public interface Marathon {
 	// Apps
 	@RequestLine("GET /v2/apps")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	GetAppsResponse getApps() throws MarathonException;
 
 	@RequestLine("GET /v2/apps/{id}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	GetAppResponse getApp(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("GET /v2/apps/{id}/tasks")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	GetAppTasksResponse getAppTasks(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("GET /v2/tasks")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	GetTasksResponse getTasks() throws MarathonException;
 
 	@RequestLine("POST /v2/apps")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	AppWithVersion createApp(App app) throws MarathonException;
 
 	@RequestLine("PUT /v2/apps/{app_id}?force={force}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	Result updateApp(@Param("app_id") String appId, App app,
 			@Param("force") boolean force) throws MarathonException;
 
 	@RequestLine("POST /v2/apps/{id}/restart?force={force}")
-	void restartApp(@Param("id") String id, @Param("force") boolean force) throws MarathonException;
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
+	Result restartApp(@Param("id") String id, @Param("force") boolean force) throws MarathonException;
 
 	@RequestLine("DELETE /v2/apps/{id}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	Result deleteApp(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("DELETE /v2/apps/{app_id}/tasks?host={host}&scale={scale}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	DeleteAppTasksResponse deleteAppTasks(@Param("app_id") String appId,
 			@Param("host") String host, @Param("scale") String scale) throws MarathonException;
 
 	@RequestLine("DELETE /v2/apps/{app_id}/tasks/{task_id}?scale={scale}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	DeleteAppTaskResponse deleteAppTask(@Param("app_id") String appId,
 			@Param("task_id") String taskId, @Param("scale") String scale) throws MarathonException;
 
 	@RequestLine("GET /v2/apps/{id}/versions")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	VersionsResponse getVersionsOfApp(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("GET /v2/apps/{id}/versions/{version}")
-	App getApp(@Param("id") String id, @Param("version") String version);
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
+	AppWithVersion getApp(@Param("id") String id, @Param("version") String version);
 
 	// Groups
 	@RequestLine("POST /v2/groups")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	Result createGroup(Group group) throws MarathonException;
 
 	@RequestLine("DELETE /v2/groups/{id}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	Result deleteGroup(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("GET /v2/groups/{id}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	Group getGroup(@Param("id") String id) throws MarathonException;
 
 	// Tasks
 
 	// Deployments
 	@RequestLine("GET /v2/deployments")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	List<Deployment> getDeployments() throws MarathonException;
 
 	@RequestLine("DELETE /v2/deployments/{deploymentId}")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	Result cancelDeploymentAndRollback(@Param("deploymentId") String id) throws MarathonException;
 
 	@RequestLine("DELETE /v2/deployments/{deploymentId}?force=true")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	void cancelDeployment(@Param("deploymentId") String id) throws MarathonException;
 
 	// Event Subscriptions
-
 	@RequestLine("POST /v2/eventSubscriptions?callbackUrl={url}")
-	public GetEventSubscriptionRegisterResponse register(@Param("url") String url) throws MarathonException;
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
+	GetEventSubscriptionRegisterResponse register(@Param("url") String url) throws MarathonException;
 
 	@RequestLine("DELETE /v2/eventSubscriptions?callbackUrl={url}")
-	public GetEventSubscriptionRegisterResponse unregister(@Param("url") String url) throws MarathonException;
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
+	GetEventSubscriptionRegisterResponse unregister(@Param("url") String url) throws MarathonException;
 
 	@RequestLine("GET /v2/eventSubscriptions")
-	public GetEventSubscriptionsResponse subscriptions() throws MarathonException;
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
+	GetEventSubscriptionsResponse subscriptions() throws MarathonException;
 
 	// Queue
 	@RequestLine("GET /v2/queue")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	QueueResponse getQueue() throws MarathonException;
 
 	// Server Info
 	@RequestLine("GET /v2/info")
+	@Headers(ClientUtils.API_SOURCE_HEADER + ": " + MarathonClient.MARATHON_CLIENT_USER_AGENT)
 	GetServerInfoResponse getServerInfo() throws MarathonException;
 
 	// Miscellaneous
-
 
 }
